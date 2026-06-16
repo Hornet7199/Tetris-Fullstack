ffe7129800c99371a4b4e98e62279fb3bd12fc86
LUCAS MACHADO FLORIANO <lucas_m_floriano@estudante.sesisenai.org.br>
2026-06-16 20:59:56 -0300

    telas adicionadas

--- tetris-frontend/src/app/app.ts
+++ tetris-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WebsocketService } from './websocket';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss' 
 })
