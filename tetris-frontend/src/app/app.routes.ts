330ea1180b237d694496b974c39fa51fceabbb7c
LUCAS MACHADO FLORIANO <lucas_m_floriano@estudante.sesisenai.org.br>
2026-06-16 21:59:51 -0300

    telas e rotas feitas

--- tetris-frontend/src/app/app.routes.ts
+++ tetris-frontend/src/app/app.routes.ts
@@ -4,5 +4,19 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./menu/menu').then(m => m.Menu)
+
+    
+  },
+
+  {
+
+    path: 'tabuleiro',
+    loadComponent: () => import('./tabuleiro/tabuleiro').then(m => m.Tabuleiro)
+  },
+  {
+   path: 'tutorial',
+    loadComponent: () => import('./tutorial/tutorial').then(m => m.Tutorial)
   }
+
+ 
 ];
--- tetris-frontend/src/app/menu/menu.ts
+++ tetris-frontend/src/app/menu/menu.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+   standalone: true,
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
 export class Menu {
+    constructor(private router: Router) {}
 
+  jogar() {
+    this.router.navigate(['/tabuleiro']);
+  }
+  tutorial(){
+    this.router.navigate(['/tutorial'])
+  }
 }
